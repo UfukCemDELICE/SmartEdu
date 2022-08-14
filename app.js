b88dd3d1a4e8deb8fbed2a4bf2c6329424cd8f62
UfukCemDELICE <ufukcemdelice02@gmail.com>
2022-08-15 01:27:25 +0300

    controllers and routes created

--- app.js
+++ app.js
@@ -1,47 +1,19 @@
 const express = require('express');
-
+const pageController = require('./controllers/pageController');
+const pageRoute = require('./routes/pageRoute')
 const app = express();
+
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
-//MIDDLEWARE
-app.use(express.static("public"));
-//ROUTES
 
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-    page_name: "index"
-  });
-});
-app.get('/about', (req, res) => {
-  res.status(200).render('about', {
-    page_name: "about"
-  });
-});
-app.get('/courses', (req, res) => {
-  res.status(200).render('courses', {
-    page_name: "courses"
-  });
-});
-app.get('/dashboard', (req, res) => {
-  res.status(200).render('dashboard', {
-    page_name: "dashboard"
-  });
-});
-app.get('/contact', (req, res) => {
-  res.status(200).render('contact', {
-    page_name: "contact"
-  });
-});
-app.get('/login', (req, res) => {
-  res.status(200).render('login', {
-    page_name: "login"
-  });
-});
-app.get('/register', (req, res) => {
-  res.status(200).render('register', {
-    page_name: "register"
-  });
-});
+//MIDDLEWARE
+app.use(express.static('public'));
+
+//ROUTES
+app.use ('/', pageRoute);
+
+
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
