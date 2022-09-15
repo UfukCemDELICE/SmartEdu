3523fb5340f159f66f9c287f264f5b59ffced538
UfukCemDELICE <ufukcemdelice02@gmail.com>
2022-09-16 00:54:48 +0300

    User model and register functions are added

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const pageController = require('./controllers/pageController');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
+
 const app = express();
+
 //CONNECT DB
 mongoose.connect('mongodb://localhost/smartEdu-db', {
     useNewUrlParser: true,
@@ -26,8 +29,10 @@ app.use(express.urlencoded({ extended: true }));
 //ROUTES
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
-
+app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
 const port = 3000;
+
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
